9b4aa5b357b975e4efa23798e4f6931da536d378
AdityaAWP <aditwpratama1@gmail.com>
2024-12-01 02:21:01 +0700

    retrive from go_export

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AdityaAWP/go-import
+
+go 1.23.2
+
+require github.com/AdityaAWP/go-export v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	exp "github.com/AdityaAWP/go-export"
+
+	"fmt"
+)
+
+func main() {
+	fmt.Println(exp.Add(2, 1))
+
+}
